ece61a329590615fe6409790666b1fc2f582ed18
SanderKeed <sanderok@stud.ntnu>
2021-03-26 23:39:40 +0100

    fixed ios

--- lib/Pages/VippsLoginPage.dart
+++ lib/Pages/VippsLoginPage.dart
@@ -8,7 +8,6 @@ import 'package:flutter/services.dart';
 import '../Screens/Authenticate/loginPage.dart';
 
 class VippsLoginPage extends StatefulWidget {
-
   @override
   _VippsLoginPageState createState() => _VippsLoginPageState();
 }
@@ -19,73 +18,96 @@ class _VippsLoginPageState extends State<VippsLoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.grey.shade100,
-        child: Center(
+        body: Container(
+      color: Colors.grey.shade100,
+      child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 400,
-                height: 400,
-                child: Material(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.white,
-                  child: Column(
-                      children: [
-                        SizedBox(height: 20,),
-                        Image(image: AssetImage('vipps/vippsLogo.png'),),
-                        SizedBox(height: 20,),
-                        Center(child: Text('Logg inn', style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,
-                        ),),),
-                        SizedBox(height: 10,),
-                        Center(
-                          child: Text('DIGI-TALT', style: TextStyle(fontSize: 12, color: Colors.grey, fontWeight: FontWeight.bold,)),
-                        ),
-                        SizedBox(height: 20,),
-                        SizedBox(
-                          width: 150,
-                          child: TextField(
-                            controller: number,
-                            decoration: new InputDecoration(labelText: "TelefonNummer"),
-                            keyboardType: TextInputType.number,
-                            inputFormatters: <TextInputFormatter>[
-                              FilteringTextInputFormatter.digitsOnly
-                            ], // Only numbers can be entered
-                          ),),
-                        SizedBox(height: 20,),
-                        SizedBox(
-                          child: ElevatedButton(
-                              child: Text(
-                                  "Neste".toUpperCase(),
-                                  style: TextStyle(fontSize: 14)
-                              ),
-                              style: ButtonStyle(
-                                  backgroundColor: MaterialStateProperty.all<Color>(Colors.deepOrange),
-                                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(18.0),
-                                      )
-                                  )
-                              ),
-                              onPressed: (){
-                                //VippsApi().getAccessToken();
-                              }
-                          ),
-                        )
-                      ]
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 400,
+            height: 400,
+            child: Material(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.white,
+              child: Column(children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Image(
+                  image: AssetImage('assets/vipps/vippsLogo.png'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: Text(
+                    'Logg inn',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              TextButton(
-                  onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));},
-                  child: Text('Avbryt', style: TextStyle(color: Colors.grey),))
-            ],
-          )
-        ),
-      )
-    );
+                SizedBox(
+                  height: 10,
+                ),
+                Center(
+                  child: Text('DIGI-TALT',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      )),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: 150,
+                  child: TextField(
+                    controller: number,
+                    decoration: new InputDecoration(labelText: "TelefonNummer"),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ], // Only numbers can be entered
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  child: ElevatedButton(
+                      child: Text("Neste".toUpperCase(),
+                          style: TextStyle(fontSize: 14)),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Colors.deepOrange),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                          ))),
+                      onPressed: () {
+                        //VippsApi().getAccessToken();
+                      }),
+                )
+              ]),
+            ),
+          ),
+          TextButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              },
+              child: Text(
+                'Avbryt',
+                style: TextStyle(color: Colors.grey),
+              ))
+        ],
+      )),
+    ));
   }
-
 }
